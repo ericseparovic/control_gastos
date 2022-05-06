684d45f893dc8f51863a0bfa921f3ce5b5e1d190
Eric Separovic <ericseparovic07@gmail.com>
2022-05-05 22:00:52 -0300

    Create modal and form component

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,25 +1,29 @@
-const ControlPresupuesto = ({presupuesto}) => {
+const ControlPresupuesto = ({ presupuesto }) => {
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
 
-    // const formatearCantidad = (cantidad) => {
-    //   return cantidad.toLocalString('en-US', {
-    //         style: 'currency',
-    //         currency:'USD'
-    //     })
-    // }
+  return (
+    <div className="contenedor-presupuesto contenedor sombra dos-columnas">
+      <div>
+        <p>Grafica aqui</p>
+      </div>
+      <div className="contenido-presupuesto">
+        <p>
+          <span>Presupuesto:</span> {formatearCantidad(presupuesto)}
+        </p>
+        <p>
+          <span>Disponible:</span> {formatearCantidad(0)}
+        </p>
+        <p>
+          <span>Gastado:</span> {formatearCantidad(0)}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="contenedor-presupuesto contenedor sombra dos-columnas">
-            <div><p>Grafica aqui</p></div>
-            <div className="contenido-presupuesto">
-                <p>
-                    <span>Presupuesto:</span> ${presupuesto}
-                </p>
-
-
-            </div>
-
-        </div>
-    )
-}
-
-export default ControlPresupuesto
+export default ControlPresupuesto;
